373efd480ee1f774e8845635141436e1453b859c
murilo <muriraubach@gmail.com>
2021-12-20 16:57:12 -0300

    feat: add sign in page design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,29 @@
-const Home = () => {
-  return <h1>Dashgo</h1>;
+import { Button, Flex, Stack } from '@chakra-ui/react';
+
+import Input from '../components/Form/Input';
+
+const SignIn = () => {
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="8">
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 };
 
-export default Home;
+export default SignIn;
