91054a0da874e011040df06649702bc52f656b60
murilo <muriraubach@gmail.com>
2021-12-25 23:21:12 -0300

    feat: add pagination in route to get users in mirage api

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import faker from 'faker';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { createServer, Factory, Model } from 'miragejs';
+import { createServer, Factory, Model, Response } from 'miragejs';
 
 type User = {
   name: string;
@@ -38,7 +38,21 @@ const makeServer = () => {
       this.namespace = 'api';
       this.timing = 750;
 
-      this.get('/users');
+      this.get('/users', function (schema, req) {
+        // eslint-disable-next-line camelcase
+        const { page = 1, per_page = 10 } = req.queryParams;
+        const total = schema.all('user').length;
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all('user')).users.slice(
+          pageStart,
+          pageEnd,
+        );
+
+        return new Response(200, { 'x-total-count': String(total) }, { users });
+      });
       this.post('/users');
 
       this.namespace = '';
