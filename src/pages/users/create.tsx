6055a7f73f38649abfb2aa81207aa610cbd0fc15
murilo <muriraubach@gmail.com>
2021-12-26 19:06:18 -0300

    feat: add a new user to mirage api with useMutations from react query

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -10,12 +10,16 @@ import {
 } from '@chakra-ui/react';
 import { yupResolver } from '@hookform/resolvers/yup';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { useMutation } from 'react-query';
 import * as yup from 'yup';
 
 import { Input } from '../../components/Form/Input';
 import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
+import api from '../../services/api';
+import queryClient from '../../services/queryClient';
 
 type CreateUserFormData = {
   name: string;
@@ -37,16 +41,35 @@ const CreateUserFormSchema = yup.object().shape({
 });
 
 const CreateUser = () => {
+  const router = useRouter();
+
+  const createUser = useMutation(
+    async (user: CreateUserFormData) => {
+      const response = await api.post('users', {
+        user: {
+          ...user,
+          created_at: new Date(),
+        },
+      });
+
+      return response.data.user;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('users');
+      },
+    },
+  );
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(CreateUserFormSchema),
   });
   const { errors } = formState;
 
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async values => {
-    await new Promise(resolve => {
-      setTimeout(resolve, 2000);
-    });
-    console.log(values);
+    await createUser.mutateAsync(values);
+
+    router.push('/users');
   };
 
   return (
